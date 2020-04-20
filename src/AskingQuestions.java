61bf72c70537f19e1f9f77410eb0146317926cf5
TaoheedKing <tking92@montgomerycollege.edu>
2020-04-20 19:34:40 -0400

    first commit

---
+++ src/AskingQuestions.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AskingQuestions{
+    public static void main( String[] args ) {
+        Scanner keyboard = new Scanner(System.in);
+        double num1, num2, num3;
+            System.out.print( "First temperature? " );
+            num1 = keyboard.nextDouble();
+
+            System.out.print( "Second temperature? " );
+            num2 = keyboard.nextDouble();
+
+            num3 = (num1 + num2)/2;
+
+            System.out.println("The max value is : " + num3);
+    }
+}
